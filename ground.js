c690cf099f29977bd306cc74e5f51a15ce15e9b5
hinriksnaer <hinriksnaer@gmail.com>
2017-10-30 15:36:42 +0000

    fixed ground collision bug

--- ground.js
+++ ground.js
@@ -6,9 +6,9 @@ function Ground(descr) {
 //global wall has a arry that stores the  wall states
 var g_ground = new Ground({
   width : 20,
-  height : 20,
-  cx : 100,
-  cy : 500,
+  height : 60,
+  cx : 300,
+  cy : 470,
 });
 
 
@@ -23,9 +23,11 @@ Ground.prototype.drawBricks = function(ctx){
 
 
 Ground.prototype.collidesWithGround = function(posX,posY,width,height){
-  if (posY+height/2 > this.cy && posY - height/2 < this.cy &&
-      (posX + width/2 > this.cx && posX - width/2 < this.cx + this.width)){
-        console.log("collided with top");
+  if (posY + height/2 > this.cy
+      && posX - width/2 < this.cx + this.width
+      && posX + width/2 > this.cx
+      && posY - height/2 < this.cy + this.height
+      ){
         return true;
       } else {
         return false;
