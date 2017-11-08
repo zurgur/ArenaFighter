edbf732ce7bf35f7c69a339f166f668c052e5ed2
zurgur <harryfreyr@gmail.com>
2017-11-08 11:39:56 +0000

    scaling fix

--- Scale.js
+++ Scale.js
@@ -3,15 +3,24 @@ var Scale ={
     var p1 = player1.getPos();
     var p2 = player2.getPos();
     //wrappedDistSq: function(x1, y1, x2, y2, xWrap, yWrap)
-    var d1 = util.wrappedDistSq(p1.posX, p1.posY, 0, g_canvas.height/2,g_canvas.width, g_canvas.height);
-    var d2 = util.wrappedDistSq(p2.posX, p2.posY, 0, g_canvas.height/2,g_canvas.width, g_canvas.height);
-    var d3 = util.wrappedDistSq(p1.posX, p1.posY, 0, 0,g_canvas.width, g_canvas.height);
-    var d4 = util.wrappedDistSq(p2.posX, p2.posY, 0, 0,g_canvas.width, g_canvas.height);
-    var d5 = util.wrappedDistSq(p1.posX, p1.posY, 0, 0,g_canvas.width, g_canvas.height);
-    var d6 = util.wrappedDistSq(p2.posX, p2.posY, g_canvas.width/2, 0,g_canvas.width, g_canvas.height);
-    var d7 = util.wrappedDistSq(p1.posX, p1.posY, g_canvas.width/4, 0,g_canvas.width, g_canvas.height);
-    var d8 = util.wrappedDistSq(p2.posX, p2.posY, (g_canvas.width/4)*3, 0,g_canvas.width, g_canvas.height);
-    var d = Math.min(d1,d2,d3,d4,d5,d6,d7,d8);
+    //check corner cases
+    var d1 = util.wrappedDistSq(p1.posX, p1.posY, 0, 0,g_canvas.width, g_canvas.height);
+    var d2 = util.wrappedDistSq(p2.posX, p2.posY, 0, 0,g_canvas.width, g_canvas.height);
+    //check midle y and x :0 : width
+    var d3 = util.wrappedDistSq(p1.posX, p1.posY, 0, g_canvas.height/2,g_canvas.width, g_canvas.height);
+    var d4 = util.wrappedDistSq(p2.posX, p2.posY, 0, g_canvas.height/2,g_canvas.width, g_canvas.height);
+    //x: width/4 and y : 0 : height
+    var d5 = util.wrappedDistSq(p1.posX, p1.posY, g_canvas.width/4, 0,g_canvas.width, g_canvas.height);
+    var d6 = util.wrappedDistSq(p2.posX, p2.posY, g_canvas.width/4, 0,g_canvas.width, g_canvas.height);
+    //x: width/2 and y : 0 : height
+    var d7 = util.wrappedDistSq(p1.posX, p1.posY, g_canvas.width/2, 0,g_canvas.width, g_canvas.height);
+    var d8 = util.wrappedDistSq(p2.posX, p2.posY, g_canvas.width/2, 0,g_canvas.width, g_canvas.height);
+    //x: (width/4)*3 and y : 0 : height
+    var d9 = util.wrappedDistSq(p1.posX, p1.posY, 3*g_canvas.width/4, 0,g_canvas.width, g_canvas.height);
+    var d10 = util.wrappedDistSq(p2.posX, p2.posY, 3*g_canvas.width/4, 0,g_canvas.width, g_canvas.height);
+
+    // the closest distans to point
+    var d = Math.min(d1,d2,d3,d4,d5,d6,d7,d8,d9,d10);
 
     return 1+(d/g_canvas.width)/160;
   },
