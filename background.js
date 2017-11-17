f4188e7dbcfcbe9acac373e6881a3687b46f4406
Ásdís Hafdísardóttir <frakkur95@gmail.com>
2017-11-17 20:59:42 +0000

    new background

--- background.js
+++ background.js
@@ -15,12 +15,12 @@ Background.prototype.drawAt = function (ctx, x, y) {
 
 Background.prototype.drawBackrond = function (ctx,du) {
 
-  //draw a box to fill the top of the image
+/*  //draw a box to fill the top of the image
   ctx.beginPath();
   ctx.rect(0, 0, 1600, 301);
   ctx.fillStyle = "#8FD0B1";
   ctx.fill();
-
+*/
  this.totalSeconds += du;
  var vx = 100; // the background scrolls with a speed of 100 pixels/sec
  var numImages = Math.ceil(g_canvas.width / this.width) + 1;
@@ -29,7 +29,7 @@ Background.prototype.drawBackrond = function (ctx,du) {
  ctx.translate(-xpos, 0);
  ctx.rect(20,20,150,100);
  for (var i = 0; i < numImages; i++) {
-     ctx.drawImage(this.image, i * this.width, 300);
+     ctx.drawImage(this.image, i * this.width, 0);
  }
  ctx.restore();
 };
