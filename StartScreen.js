a2db9e8d72fdefa549dc5e63bb3b05e1cc621251
Ásdís Hafdísardóttir <frakkur95@gmail.com>
2017-11-18 19:12:57 +0000

    Lighter Start screen image

--- StartScreen.js
+++ StartScreen.js
@@ -8,6 +8,7 @@ context.fillText("Areana Figther", canvas.width/2,canvas.height/2);
 context.font = "20px Impact";
 context.fillText("Press Enter to strart", canvas.width/2,canvas.height/2+30);
 
+
 var soundtrack = new Audio('sounds/Pixel Fags.mp3');
 /*soundtrack.addEventListener('ended', function() {
   if(g_playMusic){
