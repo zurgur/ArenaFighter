cd613891961f34f4fc2263d854b9c57e01215a52
hinriksnaer <hinriksnaer@gmail.com>
2017-11-20 19:13:36 +0000

    jump tweak

--- Player.js
+++ Player.js
@@ -149,7 +149,7 @@ Player.prototype.computeGravity = function () {
     return g_useGravity ? NOMINAL_GRAVITY : 0;
 };
 
-var NOMINAL_JUMP = 8;
+var NOMINAL_JUMP = 7.5;
 
 Player.prototype.computeThrustMag = function () {
     var thrust = 0;
